63ed08df373387cc40f3f3c766d77d95914f6e07
Lomenai <lomesame@gmail.com>
2022-04-28 08:01:22 +0900

    http vs express

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const express = require("express");
-const app = express();
+// 내장모듈
+const http = require("http");
+const app = http.createServer((req, res) => {
+  // 내가 보내는게 text중에 html이고 utf8로 해석을 해달라
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  if (req.url === "/") {
+    // 한글이 깨져서 나옴
 
-app.get("/", (req, res) => {
-  res.send("hi");
+    res.end("root입니다");
+  } else if (req.url === "/login") {
+    res.end("login입니다");
+  }
 });
+// 서버가 열려있으나 root경로가없어서 계속 도는 것
+app.listen(3001, () => {
+  console.log("http로 가동된 서버");
+});
+// const express = require("express");
+// const app = express();
 
-app.listen(3000, () => {
-  console.log("localhost:3000");
-});
+// app.get("/", (req, res) => {
+//   res.send("hi");
+// });
+// app.get("/login", (req, res) => {
+//   res.send("로그인 화면");
+// });
+// app.listen(3000, () => {
+//   console.log("localhost:3000");
+// });
