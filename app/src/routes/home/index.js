a6e51cb65f732120a0650ca60d141e09bc3e8bb6
Lomenai <lomesame@gmail.com>
2022-04-28 23:28:50 +0900

    clean folder

---
+++ app/src/routes/home/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const express = require("express");
+const ctrl = require("./home.ctrl");
+const router = express.Router();
+
+router.get("/", ctrl.home);
+
+router.get("/login", ctrl.login);
+
+module.exports = router;
