aedba685268af91e5a243fbd90678ea7e5f4808c
woosungkim0123 <sibu2005@naver.com>
2022-04-29 22:29:34 +0900

    promise res.json

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -23,5 +23,14 @@ function login() {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(req),
-  });
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
+  // promise가 날라옴
+  // res.json의 반환값은 promise이다. 기본 res의 반환값은 response 스트림인데 json() 메서드를 통해 response(응답) 스트림을 읽을 수 있다
+  // response는 데이터가 모두 받아진 상태가 아니다 json()으로 response 스트림을 가져와 완료될떄 까지 읽는다.
+  // 다읽은 body의 텍스트를 Promise 형태로 반환한다
+  // promise로 들어와서 then으로 한번더 받아서
+  // 파라미터로 넘기는 값을 함수 안에 다시 넘길때는 생략가능
+  // then(console.log) 똑같이 동작
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,9 +9,29 @@ const output = {
   },
 };
 
+const users = {
+  id: ["woosung0123", "나개발", "김팀장"],
+  psword: ["1234", "1234", "123456"],
+};
+
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const { id } = req.body,
+      { psword } = req.body;
+    console.log(users.id.includes(id));
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        // return으로 나가기 때문에 else 안써도됨
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
   },
 };
 
